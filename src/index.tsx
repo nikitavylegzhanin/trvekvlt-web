83039d323434f5af6b2a4c790c3bf801478f00bf
Nikita Vylegzhanin <vylegzhanin.nikita@gmail.com>
2022-06-04 16:49:09 +0300

    Init app

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import reportWebVitals from './reportWebVitals'
+import './index.css'
+
+const root = createRoot(document.getElementById('root') as HTMLElement)
+
+root.render(<StrictMode />)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
