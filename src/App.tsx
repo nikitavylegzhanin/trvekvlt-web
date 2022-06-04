eeba20cbed99585a58246e38bd1d9c5ab8a5aeea
Nikita Vylegzhanin <vylegzhanin.nikita@gmail.com>
2022-06-04 18:01:27 +0300

    Add @apollo/client

---
+++ src/App.tsx
@@ -0,0 +1,3 @@
+const App = () => <>trvekvlt</>
+
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ApolloProvider } from '@apollo/client'
 
 import reportWebVitals from './reportWebVitals'
+import client from './client'
+import App from './App'
 import './index.css'
 
 const root = createRoot(document.getElementById('root') as HTMLElement)
 
-root.render(<StrictMode />)
+root.render(
+  <StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
